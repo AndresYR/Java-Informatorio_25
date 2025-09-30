5b3704061c345730715034977f3ff6426e60e861
Andres Yasir Romero <andyasromero@gmail.com>
2025-09-30 18:13:04 -0300

    tp2

---
+++ actividades/tp2/Arreglos.java
@@ -0,0 +1,11 @@
+package actividades.tp2;
+
+public class Arreglos {
+    public static void main(String[] args) {
+        int[] edades = {21, 23, 25, 30, 32};
+
+        for (int edad : edades) {
+            System.out.println("Edad: " + edad);
+        }
+    }
+}
